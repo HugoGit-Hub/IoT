5d7bf726a8b341bc36dbc9022210ac51886be712
Hugo <hugo.decuq.34@icloud.com>
2023-01-05 14:48:29 +0100

    1.0

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask
+from flask import request
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+app = Flask(__name__)
+
+@app.route("/api/humidity", methods=['POST'])
+def humidity():
+    if request.method == 'POST':
+
+        frame = request.data
+        digits = getDigits(frame)
+
+        if digits == 6 :
+            value = getPayloadOne(frame)
+            writeInfluxDB("humidity", value)
+        elif digits == 10 :    
+            getPayloadOne(frame)
+            writeInfluxDB("humidity", value)
+            getPayloadTwo(frame)
+            writeInfluxDB("humidity", value)
+
+        return request.data
+
+@app.route("/api/temperature", methods=['POST'])
+def temperature():
+    if request.method == 'POST':
+
+        frame = request.data
+        digits = getDigits(frame)
+
+        if digits == 6 :
+            value = getPayloadOne(frame)
+            writeInfluxDB("humidity", value)
+        elif digits == 10 :    
+            getPayloadOne(frame)
+            writeInfluxDB("humidity", value)
+            getPayloadTwo(frame)
+            writeInfluxDB("humidity", value)
+
+        return request.data
+
+
+def getDigits(frame):
+    first = str(frame).split(":")
+    second = first[1].split("\"")
+    return len(second[1])
+
+def getCorePayload(frame):
+    first = str(frame).split(":")
+    second = first[1].split("\"")
+    return second[1]
+
+def getPayloadOne(frame) :
+    code = getCorePayload(frame)
+    return code[3:6]
+
+def getPayloadTwo(frame) :
+    code = getCorePayload(frame)
+    return int(code[7:10])
+
+def writeInfluxDB(key, value) :
+    bucket = "HD"
+    org = "HD"
+    token = "token"
+    url="localhost:8086"
+
+    client = influxdb_client.InfluxDBClient(
+        url=url,
+        token=token,
+        org=org
+    )
+
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+
+    p = influxdb_client.Point("my_measures").tag(key, value)
+    write_api.write(bucket=bucket, org=org, record=p)
